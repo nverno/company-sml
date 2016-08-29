9c6aea046205759241ea7561b1b9e3fe5a8497e9
nverno <noah.v.peart@gmail.com>
2016-08-28 21:07:42 -0400

    words

---
+++ dev/sml-capf.el
@@ -0,0 +1,11 @@
+(defgroup sml-capf nil
+  "Completion at point for SML files."
+  :group 'languages
+  :group 'sml
+  :prefix "sml-capf-")
+
+(defvar sml-capf-base-types
+  '("int" "bool" "real" "unit"))
+
+(defun sml-capf-decl-p ()
+  (let ()))
